1078d08d020a6428c0e88b068bcfa29baa621efd
singhshashank25 <shashanksinghranu@gmail.com>
2022-11-10 21:56:26 +0530

    some file deleted

--- static/js/queue.js
+++ static/js/queue.js
@@ -31,7 +31,7 @@ function printdata(data) {
                       <td><a href ="#" onclick=songtofooter("${data[i].id}")>${data[i].songName}</a></td>
                       <td>${data[i].artistsName}</td>
                       <td>add to playlist</td>
-                      <td><a class="${id}" onclick="changeColor(this.className)"> <i class="fa fa-heart-o" style="font-size:26px;color:white"></i></a></td>
+                      <td><a class="${id}" onclick="changeColor(this.className)"> <i id ="00${id}" class="fa fa-heart-o" style="font-size:26px;color:white"></i></a></td>
                       <td>${Math.floor((data[i].time) / 60000)}:${Math.floor(((data[i].time) / 1000) % 60)}</td>
                     
                     </tr>`
@@ -48,19 +48,17 @@ function printdata(data) {
 
 
 function changeColor(x) {
-    console.log(x);
-    var liked = document.getElementsByClassName("fa-heart-o");
+    // console.log(x);
+    // var liked = document.getElementsByClassName("fa-heart-o");
+    var liked = document.getElementById(`00${x}`);
     var likedColor = document.getElementsByClassName(`${x}`);
-    // likedColor[0].innerHTML = `<i class="fa fa-heart-o" style="font-size:26px;color:red"></i>`;
-    // console.log(likedColor[0]);
 
-    console.log(liked[0].outerHTML.match('white'));
-    // console.log(liked[0].innerHTML.match('white'));
-    if (liked[0].outerHTML.match('white') != null) {
-
-        likedColor[0].innerHTML = `<i class="fa fa-heart-o" style="font-size:26px;color:red"></i>`;
+    
+    if(liked.style.cssText.match('white')!=null) {
+      
+      likedColor[0].innerHTML = `<i id="00${x}" class="fa fa-heart-o" style="font-size:26px;color:red"></i>`;
     }
     else {
-        likedColor[0].innerHTML = `<i class="fa fa-heart-o" style="font-size:26px;color:white"></i>`;
+      likedColor[0].innerHTML = `<i id="00${x}" class="fa fa-heart-o" style="font-size:26px;color:white"></i>`;
     }
 }
